f33e5e13494c02523d7377e10de166bb84e13587
Grant Eckstein <grant0eckstein@gmail.com>
2022-09-17 14:05:02 -0500

    Added testing boiler

---
+++ blot_test.go
@@ -0,0 +1,22 @@
+package blot
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestBlot_Obfuscate(t *testing.T) {
+	var methods []Method
+
+	methods = append(methods, Compress())
+	methods = append(methods, Encrypt())
+
+	blot := NewBlot(methods)
+
+	input := []byte("Hello, world")
+	output := blot.Run(input)
+
+	if bytes.Compare(input, output) > 0 {
+		t.Errorf("Input %v does not match output %v", input, output)
+	}
+}
