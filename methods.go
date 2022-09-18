88361e0dbc99b1a2cd49d575e566c0863cf9481e
Grant Eckstein <grant0eckstein@gmail.com>
2022-09-17 23:09:33 -0500

    Fixed issue with encryption field and added encode/decode

--- methods.go
+++ methods.go
@@ -1,6 +1,7 @@
 package blot
 
 import (
+	"encoding/base64"
 	"github.com/Grant-Eckstein/everglade"
 	"log"
 )
@@ -33,24 +34,35 @@ func (b *Blot) Decompress() Method {
 }
 
 func (b *Blot) Encrypt() Method {
-	eg := everglade.New()
 
-	// Create method parameters
-	if b.Data == nil {
-		b.Data = make(Parameters)
+	// Keyset does not exist
+	if _, ok := b.Data["egJSON"]; !ok {
+		eg := everglade.New()
+		// Create method parameters
+		if b.Data == nil {
+			b.Data = make(Parameters)
+		}
+		b.Data["egJSON"] = eg.Export()
 	}
-	b.Data["egJSON"] = eg.Export()
 
 	return NewMethod(encryptMethodFunc, b.Data)
 }
 
 func (b *Blot) Decrypt() Method {
 	// Assert that config exists
-	if b.Data == nil {
+	if _, ok := b.Data["egJSON"]; !ok {
 		log.Fatalln("Data not set for decryption")
 	}
 
 	return NewMethod(decryptMethodFunc, b.Data)
+}
+
+func (b *Blot) Encode() Method {
+	return NewMethod(encodeBase64, b.Data)
+}
+
+func (b *Blot) Decode() Method {
+	return NewMethod(decodeBase64, b.Data)
 }
 
 /* Actual methods go here */
@@ -78,4 +90,19 @@ var decryptMethodFunc MethodFunc = func(in []byte, parameters Parameters) []byte
 	eg := everglade.Import(parameters["egJSON"])
 	pt := eg.DecryptCBC(parameters["iv"], in)
 	return pt
+}
+
+// TODO b64 encode/decode
+var encodeBase64 MethodFunc = func(in []byte, parameters Parameters) []byte {
+	dst := make([]byte, base64.StdEncoding.EncodedLen(len(in)))
+	base64.StdEncoding.Encode(dst, in)
+	return dst
+}
+
+var decodeBase64 MethodFunc = func(in []byte, parameters Parameters) []byte {
+	dst := make([]byte, base64.StdEncoding.DecodedLen(len(in)))
+	n, _ := base64.StdEncoding.Decode(dst, in)
+
+	dst = dst[:n]
+	return dst
 }
