1cd14755c06ab7109aa3bd1f29ea2d5515cb2db9
Grant Eckstein <grant0eckstein@gmail.com>
2022-09-17 22:09:38 -0500

    Updating test file

--- blot_test.go
+++ blot_test.go
@@ -2,17 +2,31 @@ package blot
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
 )
 
-func TestBlot_Obfuscate(t *testing.T) {
-	var methods []Method
+func TestBlot_Compression(t *testing.T) {
+	//var methods []Method
 
-	methods = append(methods, Compress())
-	methods = append(methods, Decompress())
-	methods = append(methods, Encrypt())
+	blot := NewBlot()
 
-	blot := NewBlot(methods)
+	blot.addMethod(blot.Compress())
+	blot.addMethod(blot.Decompress())
+
+	input := []byte("Hello,world")
+	output := blot.Run(input)
+
+	if bytes.Compare(input, output) != 0 {
+		t.Errorf("Input %v does not match output %v", input, output)
+	}
+}
+
+func TestBlot_Encrypt(t *testing.T) {
+	blot := NewBlot()
+
+	blot.addMethod(blot.Encrypt())
+	blot.addMethod(blot.Decrypt())
 
 	input := []byte("Hello, world")
 	output := blot.Run(input)
@@ -20,4 +34,29 @@ func TestBlot_Obfuscate(t *testing.T) {
 	if bytes.Compare(input, output) != 0 {
 		t.Errorf("Input %v does not match output %v", input, output)
 	}
+}
+
+func TestBlot_Export(t *testing.T) {
+	// Create test blot
+	blot := NewBlot()
+
+	blot.addMethod(blot.Encrypt())
+
+	input := []byte("Hello, world")
+	output := blot.Run(input)
+
+	// Export blot to JSON
+	j := blot.Export()
+
+	// Create new blot from export
+	blot2 := Import(j)
+	blot2.addMethod(blot2.Decrypt())
+	input2 := blot2.Run(output)
+
+	if bytes.Compare(input, input2) != 0 {
+		t.Errorf("Input %v does not match output %v", input, output)
+	}
+
+	fmt.Printf("Input: %v\n", string(input))
+	fmt.Printf("Input2: %v\n", string(input2))
 }
